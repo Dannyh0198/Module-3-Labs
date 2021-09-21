1436bf8bed5b52ac82de61374e93ce8712e55949
Danny Hayward <Dannyh0198@hotmail.co.uk>
2021-09-21 18:18:52 +0100

    Add project files.

---
+++ Module 3 Labs/Module_3_Labs.py
@@ -0,0 +1,9 @@
+# SSgt Daniel Hayward. Module 3 Labs. 21/09/21
+
+#3.1.1.4 LAB: Questions and answers
+# Using one of the comparison operators in Python, write a simple two-line program that takes the parameter n as input, 
+# which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+n = int(input ("Input N:"))
+print(n >= 100)
+
+
